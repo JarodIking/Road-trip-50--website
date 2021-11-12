fd672bc6023d536a4b879c784ec7f68b68043c41
thebestprogrammer <jarodiking17@gmail.com>
2021-11-12 09:02:41 +0100

    removed secutity issues

--- pages/registeration/send.php
+++ pages/registeration/send.php
@@ -37,7 +37,7 @@ if(!isset($_POST['hidden']) || $_POST['hidden'] === ''){
     function CheckCaptcha($userResponse) {
         $fields_string = '';
         $fields = array(
-            'secret' => "6Lf_yKYaAAAAAMcCjXk-sqDfXoOWpKn5rM23OOiA" ,
+            'secret' => "" ,
             'response' => $userResponse
         );
         foreach($fields as $key=>$value)
@@ -74,8 +74,8 @@ if(!isset($_POST['hidden']) || $_POST['hidden'] === ''){
             $mail->isSMTP();                                            // Send using SMTP
             $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-            $mail->Username   = 'roadTripInschrijving@gmail.com';                     // SMTP username
-            $mail->Password   = 'Awewf!3345gfFFvs3l';                               // SMTP password
+            $mail->Username   = '';                     // SMTP username
+            $mail->Password   = '';                               // SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
             $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
